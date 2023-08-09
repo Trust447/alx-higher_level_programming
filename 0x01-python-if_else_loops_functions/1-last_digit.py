f11cfcd99396ba0855bf44d37422ebdfcaec8921
trust <isaactrust447@gmail.com>
2023-08-10 00:47:26 +0100

    trust

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,8 +7,8 @@ if number < 0:
 print(f"Last digit of {number:d} is {last_digit:d} ", end="")
 if last_digit > 5:
     print("and is greater than 5 ")
-if last_digit == 0:
+elif last_digit == 0:
     print("and is greater than 5 ")
-if last_digit < 6 and last_digit != 0:
+else:
     print("and is less than 6 and not 0")
     print("\n")
