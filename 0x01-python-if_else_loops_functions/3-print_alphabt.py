5aaa834353416548e5cb70e4a5e3b227e9d86805
trust <isaactrust447@gmail.com>
2023-08-10 19:24:47 +0100

    trust

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for alpha_exm in range(ord('a'), ord('z')):
+for alpha_exm in range(ord('a'), ord('z') + 1):
     if alpha_exm not in [ord('e'), ord('q')]:
         print("{}".format(chr(alpha_exm)), end="")
