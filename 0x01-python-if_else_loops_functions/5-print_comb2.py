95b758b3207afe2dc8e9e32d375bec44936615fe
trust <isaactrust447@gmail.com>
2023-08-10 21:53:09 +0100

    trust

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for numbers in range(00, 100):
     if numbers != 99:
-        print("{:02}".format(numbers), end=", ")
-print("99\n")
+        print("{:02}, ".format(numbers), end="")
+    else:
+        print("{:02}".format(numbers))
