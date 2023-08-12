080eef5c93d10ff2825139e582e1516b53be062f
trust <isaactrust447@gmail.com>
2023-08-12 02:05:00 +0100

    trust

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,7 +2,8 @@
 if __name__ == '__main__':
     import sys
     total = 0
+
     for index, arg in enumerate(sys.argv, start=0):
-         if index != 0:
-             total += int(sys.argv[index])
+        if index != 0:
+            total += int(sys.argv[index])
     print("{}".format(total))
