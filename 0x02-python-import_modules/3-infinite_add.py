5f26eb1f5c82efe0a19540821857b74882d000fd
trust <isaactrust447@gmail.com>
2023-08-12 01:58:45 +0100

    trust

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-     """Print the addition of all arguments"""
-      import sys
-
-      total = 0
-      for i in range(len(sys.argv) - 1):
-          total += int(sys.argv[i + 1])
-           print("{}".format(total))
+    import sys
+    total = 0
+    for i in range(len(sys.argv) - 1):
+         if index != 0:
+             total += int(sys.argv[index])
+    print("{}".format(total))
