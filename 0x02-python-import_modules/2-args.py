ae821e3a9cc55ad858f47101d8470cc6ad7ce03e
trust <isaactrust447@gmail.com>
2023-08-12 01:53:46 +0100

    trust

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,11 +4,11 @@ if __name__ == "__main__":
 
     count = len(sys.argv) - 1
     if count == 0:
-          sufix = "arguments."
+        sufix = "arguments."
     elif count == 1:
-           sufix = "argument:"
+        sufix = "argument:"
     else:
-          sufix = "arguments:"
+        sufix = "arguments:"
     print("{} {}".format(count, sufix))
     for index, arg in enumerate(sys.argv, start=0):
         if index != 0:
