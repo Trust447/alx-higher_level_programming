a9ea4c75e0fb1ae015bebb70453834dafc31260a
trust <isaactrust447@gmail.com>
2023-08-14 23:48:11 +0100

    trust

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     if my_list:
-        if 0 <= idx and idx < len(my_list):
+        if 0 <= idx < len(my_list):
             return my_list[idx]
         else:
             return none
