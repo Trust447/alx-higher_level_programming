02484922fa7b6f206cbfcbfd91c7efa2f5201d5d
trust <isaactrust447@gmail.com>
2023-08-15 00:56:37 +0100

    trust

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,5 +3,5 @@ def print_reversed_list_integer(my_list=[]):
     if my_list:
         my_list.reverse
         for items in my_list
-        print("{}".format(items)
+        print("{:d}".format(items))
 
