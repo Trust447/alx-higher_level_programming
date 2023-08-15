6e9597f65ee27c5542ab6e746b3da0fe8620b94b
trust <isaactrust447@gmail.com>
2023-08-15 01:22:40 +0100

    trust

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     if my_list:
+        my_list_copy = my_list.copy()
         if (0 > idx) or (idx >= len(my_list)):
-            return my_list
+            return my_list_copy
         else:
             my_list[idx] = element
             return my_list
