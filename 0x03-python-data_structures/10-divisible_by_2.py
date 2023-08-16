d083db1db6e7eb0982bd60f1d5ab1e5b99727d5e
trust <isaactrust447@gmail.com>
2023-08-16 03:09:22 +0100

    trust

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    new_list = []
+    for items in range(len(my_list)):
+            my_list[items] % 2 == 0:
+            new_list.append(True)
+        else:
+            new.append(False)
+    return new
+
+
