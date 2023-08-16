a1fb1ff0f7a71a397b334716f7e7b1c863f109ab
trust <isaactrust447@gmail.com>
2023-08-17 00:25:00 +0100

    trust

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if my_list:
-        if search < len(my_list):
-            my_list[search] = replace
-    return my_list[search]
+    while index < len(my_list):
+        if my_list[index] == search:
+            my_list[index] = replace
+        index += 1
+    return my_list
