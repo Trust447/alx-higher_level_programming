6127eb018e9c629f83dc87f45501ae80e4cdb6b0
trust <isaactrust447@gmail.com>
2023-08-16 01:05:02 +0100

    trust

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix:
+        for row in matrix:
+            for cell in row:
+                print("{}".format(cell), end=" ")
+        print()
