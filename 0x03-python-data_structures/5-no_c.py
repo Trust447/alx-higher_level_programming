fc4da89b5fe0fc0642e506e443838145086865cd
trust <isaactrust447@gmail.com>
2023-08-16 02:19:23 +0100

    trust

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,5 @@
 #!/usr/bin/pytthon3
 def no_c(my_string):
-    for char in my_string:
-        if char == 'c':
-            my_string.translate({ord('c'): None})
-        if char == 'C':
-            my_string.translate({ord('C'): None})
-             
-    return my_string
+    translate_char = str.maketrans('', '', 'cC')
+    new_string = my_string.translate(translate_char)
+    return new_string
