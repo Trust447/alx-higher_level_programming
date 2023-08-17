360b3282ea8f65376d980333c86dea775c105c52
trust <isaactrust447@gmail.com>
2023-08-17 01:02:35 +0100

    trust

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    common = []
+    for items in set_1:
+        if items in set_2:
+            common.append(items)
+    return diff_elements
