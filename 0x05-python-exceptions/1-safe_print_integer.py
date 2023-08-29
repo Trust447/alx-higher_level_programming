1fbda17560218e7c9ceffe267c0be2141ddc0b2b
trust <isaactrust447@gmail.com>
2023-08-29 22:32:35 +0100

    function that prints x elements of a list.

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        isinstance(value, int):
-            print("{:d}".format(value))
-            return true
-    except Exception:
-        return false
+        print("{:d}".format(value))
+        return True
+    except:
+        return False
