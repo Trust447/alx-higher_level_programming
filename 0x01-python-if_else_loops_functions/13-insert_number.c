1b8f8b50cd8011493fa4f24f159a644cff255d50
trust <isaactrust447@gmail.com>
2023-08-30 14:42:30 +0100

    go

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -2,14 +2,13 @@
 #include <stdlib.h>
 /**
 * insert_node - short description
+* Description: long description
 *
-  Description: long description
- *
- * @head: argument_1 description
- * @number: argument_2 description
- *
- * Return: return description
- */
+* @head: argument_1 description
+* @number: argument_2 description
+*
+* Return: return description
+*/
 listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *new_node, *temp = *head;
