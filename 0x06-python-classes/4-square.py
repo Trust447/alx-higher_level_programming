8ddb82c5249f6ecfad79b201f634df94fc3ed544
trust <isaactrust447@gmail.com>
2023-10-07 15:55:52 +0100

    classes

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""module that defines a rectangle"""
+
+
+class Square:
+
+    def __init__(self, size=0):
+        """initializing attributes"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """method to get value of object"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """method to set value of object"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if size > 0:
+            raise ValueError("size must be >= 0")
+    (self.__size=value)
