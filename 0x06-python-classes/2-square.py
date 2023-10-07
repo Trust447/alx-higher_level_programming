34d1d1798b29a4526cfc5f42e5369f165f38c229
trust <isaactrust447@gmail.com>
2023-10-07 12:52:15 +0100

    tr

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,13 +3,15 @@
 """module that defines a square"""
 
 class Square:
-"""A class that represents a square"""
+
+
+    """A class that represents a square"""
 
     def __init__(self, size=0):
         """initializing attributes"""
         self.__size = size
 
-    if type(size) is not int:
-        raise TypeError("size must be an integer")
-    if size < 0:
-        raise ValueError("size must be >= 0")
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
