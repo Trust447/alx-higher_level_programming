edf7adcba9a15d0d8e4e23a8e07d153a5f77cecb
trust <isaactrust447@gmail.com>
2023-10-07 13:12:59 +0100

    tr

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
 
+"""This represents a rectangle"""
+
 
 class Square:
+
+    """"this is a class of  squarea"""
     def __init__(self, size=0):
-        if type(size) is int:
-            if size < 0:
-                raise ValueError('size must be >= 0')
-            else:
-                self.__size = size
-        else:
-            raise TypeError('size must be an integer')
-    
+        """"initializing attribute"""
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
     def area(self):
+
+        """function/method that squares up the size of the square"""
         return (self.__size ** 2)
