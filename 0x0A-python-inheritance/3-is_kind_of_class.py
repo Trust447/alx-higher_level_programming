c83c06109a55b1e9ed65e09ceda20bf6bd453987
trust <isaactrust447@gmail.com>
2023-10-10 03:46:24 +0100

    3-is_kind_of_class.py

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -16,4 +16,4 @@ def is_kind_of_class(obj, a_class):
     of a class that inherited from,
     the specified class;
     otherwise False."""
-    return type(obj) is a_class
+    return isinstance(obj, a_class)
