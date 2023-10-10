5eb276e31c5d3096c6f8e59888f0bf929b55a8f9
trust <isaactrust447@gmail.com>
2023-10-10 22:15:41 +0100

    jason

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -6,9 +6,11 @@ module that defines a function that returns an object
 (Python data structure) represented by a JSON string
 """
 
+import json
+
 
 def from_json_string(my_str):
 
     """function that returns an object
     (Python data structure) represented by a JSON string"""
-    return jason.load(my_str)
+    return json.load(my_str)
