77505ee9977cbdef3e6058305a7d28c100aa6220
trust <isaactrust447@gmail.com>
2023-10-10 03:31:31 +0100

    2-is_same_class.py

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -8,4 +8,4 @@ a module that defines a function/method that checks for class type
 
 def is_same_class(obj, a_class):
     """method that checks the class type of an object/instance"""
-    return isinstance(obj, a_class)
+    return type(obj) is a_class
