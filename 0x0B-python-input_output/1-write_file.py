99ce3e209f2e2d1afad2a5d91671add0f8f3c3ac
trust <isaactrust447@gmail.com>
2023-10-10 16:48:22 +0100

    1-write_file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,15 @@
+#!/usr/python3
+
+
+"""
+module that defines a function that writes into a file
+"""
+
+
+def write_file(filename="", text=""):
+
+    """function that writes a string to a text file
+    and returns the number of characters written"""
+    with open(filename, 'w', encoding="UTF-8") as fil:
+        f = fil.write(text)
+        return f
