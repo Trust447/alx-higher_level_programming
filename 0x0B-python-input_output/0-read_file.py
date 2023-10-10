5e21498e1c42e24693e20c3e3a987f4447e72ddb
trust <isaactrust447@gmail.com>
2023-10-10 16:28:14 +0100

    read_file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+module that defines a function that read text files and prints them out
+"""
+
+
+def read_file(filename=""):
+
+    """a function that reads text files and prints it to stdout"""
+    with open(filename, encoding='utf-8') as fil:
+        f = fil.read()
+    print(f, end=" ")
