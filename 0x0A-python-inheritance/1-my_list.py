f57fe7d48d7ebee02aa4fd804617d80739c48dde
trust <isaactrust447@gmail.com>
2023-10-10 03:15:30 +0100

    0-lookup

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+"""
+module with a method that prints lists in sorted form
+"""
+
+
+class MyList(list):
+
+    """method that prints lists in a sorted form"""
+    def print_sorted(self):
+        print(sorted(self))
