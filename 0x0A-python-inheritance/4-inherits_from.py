9aa46b3a1709debd169e5de156485084ad53eb7b
trust <isaactrust447@gmail.com>
2023-10-10 04:04:07 +0100

    4-inherits_from

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+module that defines a function that checks if
+an object is an instance of a class
+"""
+
+
+def inherits_from(obj, a_class):
+
+    """function that checks if
+    an object is an instance of a class"""
+    return issubclass(type(obj), a_class)
