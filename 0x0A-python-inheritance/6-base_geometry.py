b5306863f54410984cc4b6bc296e5a325331fbff
trust <isaactrust447@gmail.com>
2023-10-10 04:35:09 +0100

    6-base_geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+"""
+module that defines a BaseGeometry class
+"""
+
+
+class BaseGeometry:
+
+    """geometry class"""
+
+    def area(self):
+
+        """method that raises an exception"""
+        raise TypeError("def area(self)")
