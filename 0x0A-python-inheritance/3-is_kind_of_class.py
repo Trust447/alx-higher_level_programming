fdc6d124ae3453767a003133dccfca865ff7f553
trust <isaactrust447@gmail.com>
2023-10-10 03:43:36 +0100

    3-is_kind_of_class.py

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -11,8 +11,8 @@ otherwise False.
 
 def is_kind_of_class(obj, a_class):
 
-    """function that returns True if the object is a
-    n instance of, or if the object is an instance
+    """function that returns True if the object is an
+    instance of, or if the object is an instance
     of a class that inherited from,
     the specified class;
     otherwise False."""
