c1358580d23b9872da41af90f874ab325bfee478
trust <isaactrust447@gmail.com>
2023-10-10 03:42:30 +0100

    2-is_same_class.py

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+"""
+module that defines a function that returns True
+if the object is an instance of,
+or if the object is an instance of a class that
+inherited from, the specified class;
+otherwise False.
+"""
+
+
+def is_kind_of_class(obj, a_class):
+
+    """function that returns True if the object is a
+    n instance of, or if the object is an instance
+    of a class that inherited from,
+    the specified class;
+    otherwise False."""
+    return type(obj) is a_class
