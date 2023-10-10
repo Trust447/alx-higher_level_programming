2b37156bff1be42c038afb7aae6e624e6fb5fd97
trust <isaactrust447@gmail.com>
2023-10-10 02:35:41 +0100

    0-lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
-"""defining method to retrive all attributes of a class in 
+"""defining method to retrive all attributes of a class in
     a list form
 """
-    
+
+
 def lookup(obj):
 
     """method to display all the attribute of a class in a list"""
-
     return [dir(obj)]
