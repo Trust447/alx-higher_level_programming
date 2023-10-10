b07cf8af48c1049240a7af74260182ea9cb06335
trust <isaactrust447@gmail.com>
2023-10-10 22:01:54 +0100

    jason

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -10,5 +10,5 @@ def append_write(filename="", text=""):
 
     """function that appends a string at the end of a text file
     and returns the number of characters added"""
-    with open(filename, "a" encoding="utf-8") as fil:
-        return fill.append(text)
+    with open(filename, "a", encoding="utf-8") as fil:
+        return fill.write(text)
