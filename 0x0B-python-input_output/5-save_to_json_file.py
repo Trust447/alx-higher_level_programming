5b7dacf20dca2f996c70ec9735a986571141ac5b
trust <isaactrust447@gmail.com>
2023-10-11 03:20:21 +0100

    jason

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -16,4 +16,4 @@ def save_to_json_file(my_obj, filename):
     Object to a text file, using a JSON representation"""
 
     with open(filename, "w") as fil:
-        json.dump(j_obj, fil)
+        return json.dump(j_obj, fil)
