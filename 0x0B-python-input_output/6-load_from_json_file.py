19f8db7a2fb6d9aaf4927b549b2e60a7a1b0593d
trust <isaactrust447@gmail.com>
2023-10-11 03:34:41 +0100

    jason

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
-
-
 """
 module that defines a function
 that creates an Object from a JSON file
 """
-
 import json
 
 
 def load_from_json_file(filename):
 
-     """Creates a Python object from a given JSON file"""
-    with open(filename) as f:
+    """Creates a Python object from a given JSON file"""
+    with open(filename "r") as f:
         return json.load(f)
-
