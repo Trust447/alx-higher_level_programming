fd1c6b91d3c6e3e0340aa14ff5b58c610e6bbd87
trust <isaactrust447@gmail.com>
2023-10-11 03:56:01 +0100

    jason

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 import json
-
-
 """
 module that defines a function that writes an
 Object to a text file, using a JSON representation
@@ -9,8 +7,6 @@ Object to a text file, using a JSON representation
 
 
 def save_to_json_file(my_obj, filename):
-
-    """function that writes an
-    Object to a text file, using a JSON representation"""
+    """function that writes anObject to a text file"""
     with open(filename, "w") as f:
         return json.dump(my_obj, f)
