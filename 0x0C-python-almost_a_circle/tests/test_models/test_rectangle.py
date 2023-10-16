faae6ac6eb1747683139aa70d3eb7269c9701483
trust <isaactrust447@gmail.com>
2023-10-17 00:02:25 +0100

    0x0C-python-almost_a_circle

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -13,5 +13,12 @@ class Rec_test(unittest.TestCase):
          rect2 = Rectangle(10, 2)
          self.assertEqual(rect2.id, 1)
 
+         def tearDown(self):
+             self.rect.width = 10
+             self.rect.height = 2
+             self.rect.x = 0
+             self.rect.y = 0
+             self.rect.id = 12
+
 if __name__ == '__main__':
     unittest.main()
