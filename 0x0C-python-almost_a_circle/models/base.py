56b6a66658ec51c76b5b032ae2c5228a3c144998
trust <isaactrust447@gmail.com>
2023-10-17 13:42:02 +0100

    updated the base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -25,9 +25,55 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
 
-        """method that returns the JSON string
-        representation of list_dictionaries"""
+        """returns JSON string representation of list_dictionaries"""
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+        def save_to_file(cls, list_objs):
+            """write a list of objects to a file as json"""
+            f_name = cls.__name__
+            if list_objs is None or len(list_objs) == 0:
+                result = "[]"
+            else:
+                my_list = []
+                for i in list_objs:
+                    my_list.append(i.to_dictionary())
+                    result = cls.to_json_string(my_list)
+            with open(f"{f_name}.json", 'w') as f:
+                f.write(result)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """returns a list from a json string"""
+        if json_string is None:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """creates a new object from the values in dictionary."""
+        if cls.__name__ == "Rectangle":
+            obj = cls(2, 6)
+        else:
+            obj = cls(2)
+        obj.update(args=None, **dictionary)
+        return obj
+
+    @classmethod
+    def load_from_file(cls):
+        """Creates a new instance of the class from the contents of a file."""
+        filename = f"{cls.__name__}.json"
+        list_objs = []
+        if not os.path.exists(filename):
+            return []
+        else:
+            with open(filename, 'r', encoding='utf-8') as file:
+                json_string = file.read()
+            list_dictionaries = Base.from_json_string(json_string)
+            for dictionary in list_dictionaries:
+                obj = cls.create(**dictionary)
+                list_objs.append(obj)
+            return list_objs
