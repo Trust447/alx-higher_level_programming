6a04a9d312c3ff010303a2f0e2491a697b7780b5
trust <isaactrust447@gmail.com>
2023-10-25 06:30:17 +0100

    inheritance

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -6,5 +6,5 @@ and methods of an object
 
 
 def lookup(obj):
-     """This functions looks out for all attributes and methods of an object"""
-     return dir(obj)
+    """This functions looks out for all attributes and methods of an object"""
+    return dir(obj)
