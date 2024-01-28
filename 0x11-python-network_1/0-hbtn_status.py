542454af6b8975dca056a2369c5e4098a92b130c
trust <isaactrust447@gmail.com>
2024-01-28 17:02:56 +0100

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
         body = res.read()
         '''Convert the response content to UTF-8 string'''
         utf8_content = body.decode('utf-8')
-        print("Body response:$")
-        print("\t- type: {}$".format(type(body)))
-        print("\t- content: {}$".format(body))
-        print("\t- utf8 content: {}$".format(utf8_content))
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(utf8_content))
