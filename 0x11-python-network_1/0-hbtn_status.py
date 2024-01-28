3e3cd9bca6d660d3f14a64672617399907179661
trust <isaactrust447@gmail.com>
2024-01-28 16:23:20 +0100

    0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+import urllib.request
+"""
+Write a Python script that fetches https://alx-intranet.hbtn.io/status
+"""
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        body = res.read()
+
+    '''Convert the response content to UTF-8 string'''
+    utf8_content = body.decode('utf-8')
+
+    print('Body response:')
+    print("     - type: {}".format(type(body)))
+    print("     - content: {}".format(body))
+    print("     - utf8 content: {}".format(utf8_content))
