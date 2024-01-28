fc91956d81f1602dc0e314a26ed93ef6b41587de
trust <isaactrust447@gmail.com>
2024-01-28 16:51:54 +0100

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,9 +9,9 @@ if __name__ == "__main__":
     request = urllib.request.Request('https://alx-intranet.hbtn.io/status')
     with urllib.request.urlopen(request) as res:
         body = res.read()
-        '''Convert the response content to UTF-8 string'''
+    '''Convert the response content to UTF-8 string'''
         utf8_content = body.decode('utf-8')
         print("Body response:")
-        print("/t- type: {}".format(type(body)))
-        print("/t- content: {}".format(body))
-        print("/t- utf8 content: {}".format(utf8_content))
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(utf8_content))
