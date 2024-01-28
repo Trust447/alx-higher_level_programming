7db25e08a1eafd41feaa96e4e5950efca2b21c9d
trust <isaactrust447@gmail.com>
2024-01-28 22:13:06 +0100

    1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,12 +7,9 @@ import sys
 
 
 
-if len(sys.argv) != 2:
-    sys.exit(1)
-
-URL = sys.argv[1]
-
 if __name__ == "__main__":
+    URL = sys.argv[1]
+
     url = urllib.request.Request(URL)
     with urllib.request.urlopen(url) as Response:
         Id = Response.headers.get("X-Request-Id")
